fb1b1c123a8650af894fc6a8ae0eda1afa155ae0
Dalibor Janák <dalibor.janak@gmail.com>
2014-12-17 15:54:50 +0100

    Fixed bug with advandced compilation, pagesize detection

--- src/exports.js
+++ src/exports.js
@@ -14,8 +14,18 @@ goog.require('klokantech.IiifPrint');
 
 goog.exportSymbol('IiifViewer', klokantech.IiifViewer);
 goog.exportSymbol('IiifViewer.prototype.getMap',
-                  klokantech.IiifViewer.prototype.getMap);
+        klokantech.IiifViewer.prototype.getMap);
 
 goog.exportSymbol('IiifSource', klokantech.IiifSource);
 
 goog.exportSymbol('IiifPrint', klokantech.IiifPrint);
+goog.exportSymbol('IiifPrint.prototype.addText',
+        klokantech.IiifPrint.prototype.addText);
+goog.exportSymbol('IiifPrint.prototype.addBase64Image',
+        klokantech.IiifPrint.prototype.addBase64Image);
+goog.exportSymbol('IiifPrint.prototype.addMap',
+        klokantech.IiifPrint.prototype.addMap);
+goog.exportSymbol('IiifPrint.prototype.addRectangle',
+        klokantech.IiifPrint.prototype.addRectangle);
+goog.exportSymbol('IiifPrint.prototype.save',
+        klokantech.IiifPrint.prototype.save);
